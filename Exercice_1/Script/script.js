f7180b8ddbdfca44430c8df73795776d6f082ecb
Ugo <ugoquem@gmail.com>
2024-11-06 16:51:39 +0000

    Les fonctions JS

---
+++ Exercice_1/Script/script.js
@@ -0,0 +1,13 @@
+document.write("Le produit de 3 x 8 est égal à " + produit (3, 8) + ".")
+
+function produit(x, y) {
+    let res = x*y;
+    return res;
+}
+
+document.write("<img src ="+afficheImg(papillon.jpg)+"></img>")
+
+function afficheImg(image) {
+    return image;
+}
+
