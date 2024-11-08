c963b791030c816b5835f9f5a4480ef22a9b0f75
Ugo Quéméneur <ugoquem@gmail.com>
2024-11-08 11:56:31 +0100

    exercice final

---
+++ Exercice final/Script/script.js
@@ -0,0 +1,30 @@
+alert("aled");
+
+const personnages = [ 
+    { nom: "Luke Skywalker", espece: "Humain", affiliation: "Jedi" }, 
+    { nom: "Darth Vader", espece: "Humain", affiliation: "Sith" }, 
+    { nom: "Yoda", espece: "Yoda's species", affiliation: "Jedi" }, 
+    { nom: "Obi-Wan Kenobi", espece: "Humain", affiliation: "Jedi" } 
+]; 
+
+function filtrePerso(affiliation) {
+
+}
+
+function rechercherPerso(nom) {
+
+}
+
+function totalPerso(nombre) {
+
+}
+
+function verifierPerso(présent) {
+
+}
+
+filtrePerso(Jedi);
+rechercherPerso(Yoda);
+totalPerso();
+verifierPerso(Luke_Skywalker);
+
